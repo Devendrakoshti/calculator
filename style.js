c2dbcc75605e8f4feb698dc2a1cec47372839d9b
Devendrakoshti <124948510+Devendrakoshti@users.noreply.github.com>
2023-05-23 13:01:12 +0530

    11

--- style.js
+++ style.js
@@ -1,2 +1,11 @@
-document.querySelector(".number")
+h1 = document.querySelector("#number")
+h2 = document.getElementById("top")
 console.log(".number")
+console.log("top")
+
+// function test(e) {
+//     h2.value = e;
+// }
+function test() {
+    h2.value = h1.value
+}
